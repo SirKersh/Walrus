01ef796d2c9b8e4d0f8b85ce835c9ccd80255eef
Deacide <carlind0@students.rowan.edu>
2016-11-22 13:27:00 -0500

    Created Prototype v0.4.1 and added multiple log file parsing through a folder

---
+++ Prototype v0.4.1/src/main/java/driver/Driver.java
@@ -0,0 +1,128 @@
+package driver;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import ruleContainer.RuleManager;
+import ruleContainer.KieManager;
+import ruleContainer.RuleCreator;
+import ruleContainer.RuleTextEditor;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		Prototype x = new Prototype();
+		Scanner scanner = new Scanner(System.in);
+		RuleManager rManager = new RuleManager();
+	
+			try {
+				long start = System.currentTimeMillis();
+				KieManager.loadAllRules();
+				long end = System.currentTimeMillis();
+				System.out.println((end-start) + "ms");
+				System.out.println((end - start) / 1000 + "s");
+			} catch (FileNotFoundException e2) {
+				System.out.println("Unable to load drools files.");
+			}
+			
+			int option = 0;
+			System.out.println("Welcome to the Rules Engine Prototype.");
+			while (option != 10) {
+
+				System.out.println("Please input the number of what you would like to do.");
+				System.out.println("1. Create a new rule.");
+				System.out.println("2. Select a Logfile.");
+				System.out.println("3. Select a folder containing multiple logfiles");
+				System.out.println("4. Add new data.");
+				System.out.println("5. Run through one line of the LogFile.");
+				System.out.println("6. Run through entire LogFile.");
+				System.out.println("7. List all active rules.");
+				System.out.println("8. List all inactive rules.");
+				System.out.println("9. Toggle rules on/off.");
+				System.out.println("10. Open Rules Editor");
+				System.out.println("11. Exit program.");
+
+				String s = scanner.nextLine();
+				try{
+					option = Integer.parseInt(s);
+				} catch (NumberFormatException e){
+					option = -1;
+				}
+				switch (option) {
+				case 1:
+					if(x.checkForLogFile())
+					{
+						System.out.println("Please input a filename.");
+						String filename = scanner.nextLine();
+						RuleCreator rf = new RuleCreator(filename, "src/main/resources/rules", scanner, x.getDataObjCol(),x.getNewData());
+						x.createNewRules(rf,rManager);
+					}
+					else
+						System.out.println("No Logfiles or Data in the system. Please choose option 2 to load log files.");
+					break;
+				case 2:
+					try {
+						x.importLogFile(scanner);
+					} catch (IOException e) {
+						System.out.println("Unable to locate LogFile.");
+					}
+					break;
+				case 3:
+					try {
+						x.importLogFiles(scanner);
+					} catch (IOException e) {
+						System.out.println("Unable to locate LogFile.");
+					}
+					break;
+				case 4:
+					try {
+						x.importNewData(scanner);
+					} catch (IOException e1) {
+						System.out.println("Unable to locate LogFile.");
+					}
+					break;
+				case 5:
+					try {
+						x.updateOnce();
+					} catch (IOException e) {
+						System.out.println("The LogFile cannot be read.");
+					}
+					break;
+				case 6:
+					try {
+						x.updateAll();
+					} catch (IOException e) {
+						System.out.println("The LogFile cannot be read.");
+					}
+					break;
+				case 7:
+					System.out.println(rManager.displayAllActiveRules());
+					break;
+				case 8:
+					System.out.println(rManager.displayAllInActiveRules());
+					break;
+				case 9:
+					try {
+						rManager.toggleRules(scanner);
+					} catch (FileNotFoundException e) {
+						System.out.println("The rule indicated cannot be found.");
+					}
+					break;
+				case 10:
+					new RuleTextEditor();
+					break;
+				case 11:
+					System.out.println("End of Program");
+					break;
+				default:
+					System.out.println("Invalid Input.");
+					break;
+				}
+				System.out.println();
+			}
+			scanner.close();
+
+	
+	}
+
+}
---
+++ Prototype v0.4.1/src/main/java/driver/Prototype.java
@@ -0,0 +1,230 @@
+package driver;
+
+import org.kie.api.runtime.KieSession;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import parser.*;
+import ruleContainer.RuleManager;
+import ruleContainer.KieManager;
+import ruleContainer.RuleCreator;
+import java.util.HashMap;
+import java.util.Iterator;
+
+
+
+public class Prototype {
+	DataObjectCollectionArrayList dataObjCol;
+	DataObjectCollectionArrayList newData;
+	KieSession kSession;
+
+	public Prototype()
+	{
+		dataObjCol = new DataObjectCollectionArrayList();
+		newData = new DataObjectCollectionArrayList();
+	}
+
+	public void createNewRules(RuleCreator rf,RuleManager ruleEditor)
+	{
+		try {
+			String fileName;
+			fileName = rf.createRule();
+			ruleEditor.addRule(fileName);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void importLogFile(Scanner scanner) throws IOException
+	{
+		Parser parser = new Parser();
+		System.out.println("Please input the csv filename");
+		String csvFile = scanner.nextLine();
+
+		while (!(new File("Logs\\" + csvFile + ".csv").exists())&&!(csvFile.equals("EXIT"))) {
+			System.out.println("No file found!");
+			System.out.println("Please input the csv filename or enter 'EXIT' to leave");
+			csvFile = scanner.nextLine();
+
+		}
+		if(csvFile.equals("EXIT"))
+		{
+			System.out.println("No file has been imported.");
+
+		}
+		else if(dataObjCol.getObject(csvFile) != null)
+		{
+			System.out.println("The system already contains the logfile " + csvFile + ".csv");
+		}
+		else
+		{
+			dataObjCol.add(parser.parse(csvFile + ".csv"));
+			System.out.println("LogFile " + csvFile + ".csv has been imported.");
+		}
+	}
+
+	public void importLogFiles(Scanner scanner) throws IOException
+	{
+		System.out.println("Please input the folder filename");
+		String folderName = scanner.nextLine();
+
+
+		while (!(new File(folderName).exists())&&!(folderName.equals("EXIT"))) {
+			System.out.println("No file found!");
+			System.out.println("Please input the folder name or enter 'EXIT' to leave");
+			folderName = scanner.nextLine();
+		}
+		if(folderName.equals("EXIT"))
+		{
+			System.out.println("No file has been imported.");
+		}
+		else
+		{
+			for (final File fileEntry : new File(folderName).listFiles()) {
+				if(dataObjCol.getObject(fileEntry.getName().substring(0, fileEntry.getName().length() - 4)) != null)
+				{
+					System.out.println("The system already contains the logfile " + fileEntry.getName());
+				}
+				else
+				{
+					Parser parser = new Parser();
+					System.out.println("LogFile " + fileEntry.getName() + " has been imported.");
+					dataObjCol.add(parser.parse(fileEntry.getName()));
+				}
+			}
+
+		}
+	}
+
+	public void importNewData(Scanner scanner) throws IOException
+	{
+		DataObject x;
+		HashMap<String, String> hm = new HashMap<String,String>();
+		int no = -1;
+
+		System.out.println("Input a file name of the new Data.");
+		String name = scanner.nextLine();
+		while(no == -1){
+			System.out.println("How many fields do you want?");
+
+			try{
+				no = Integer.parseInt(scanner.nextLine());
+			} catch (NumberFormatException e){
+				System.out.println("Please enter a valid integer.");
+				no = -1;
+			}
+		}
+		for(int i = 0;i<no;i++)
+		{
+			System.out.println("Please name the field");
+			String feild = scanner.nextLine();	
+			hm.put(feild, "");
+		}
+
+		x = new DataObject(hm, name, null);
+		newData.add(x);
+	}
+
+	public void updateAll() throws IOException
+	{
+
+		if (dataObjCol.size() > 0) {
+			while(dataObjCol.size()>0)
+			{
+				System.out.println("Fire Rules: ");
+				kSession = KieManager.kBase.newKieSession();
+				kSession.insert(dataObjCol);
+				kSession.fireAllRules();
+				kSession.dispose();
+
+				try {
+					Thread.sleep(1000);
+					System.out.println("\n");
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				// Update the specific parser
+				Iterator<DataObject> it = dataObjCol.getCollection().iterator();
+				while(it.hasNext())
+				{
+					DataObject obj = it.next();
+					if(!(obj instanceof ActionObject))
+					{
+						boolean x = obj.getParser().update(obj);
+						if (x == true)
+							it.remove();	
+					}
+					else
+					{
+						it.remove();
+					}
+
+				}
+			} 
+		}else
+			System.out.println("No Logfile Selected.");
+	}
+
+	public void updateOnce() throws IOException
+	{
+		if (dataObjCol.size() > 0) {
+
+			kSession = KieManager.kBase.newKieSession();
+			// For testing purposes only
+
+			kSession.insert(dataObjCol);
+
+			// Fire the rules engine
+			kSession.fireAllRules();
+
+			// Trash the rules
+			kSession.dispose();
+
+			// Update the specific parser
+			Iterator<DataObject> it = dataObjCol.getCollection().iterator();
+			while(it.hasNext())
+			{
+				DataObject obj = it.next();
+				if(!(obj instanceof ActionObject))
+				{
+					boolean x = obj.getParser().update(obj);
+					if (x == true)
+						it.remove();	
+				}
+				else
+				{
+					it.remove();
+				}
+
+			}
+
+		} else
+			System.out.println("No Logfile Selected.");
+	}
+
+
+	public boolean checkForLogFile()
+	{
+		return (dataObjCol.size()>0);
+	}
+
+	public DataObjectCollectionArrayList getDataObjCol()
+	{
+		return dataObjCol;
+	}
+
+	public boolean checkNewData()
+	{
+		return (newData.size()>0);
+	}
+
+	public DataObjectCollectionArrayList getNewData()
+	{
+		return newData;
+	}
+
+}
